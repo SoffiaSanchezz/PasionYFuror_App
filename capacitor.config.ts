5ff4e422cf6270b4c38a991ea0a1c97ae4105d01
SoffiaSanchezz <ssofiiadayanna@gmail.com>
2026-03-04 01:45:10 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pasion_y_Furor',
+  webDir: 'www'
+};
+
+export default config;
