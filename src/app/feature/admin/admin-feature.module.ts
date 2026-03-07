4b2e44ab70d5f05ae10dd476d08419e788804240
SoffiaSanchezz <ssofiiadayanna@gmail.com>
2026-03-07 18:24:03 -0500

    Actualizacion de el routing

---
+++ src/app/feature/admin/admin-feature.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { AdminFeatureRoutingModule } from './admin-feature-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IonicModule,
+    AdminFeatureRoutingModule
+  ]
+})
+export class AdminFeatureModule { }
