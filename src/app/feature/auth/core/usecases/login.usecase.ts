67142b468f8ecdf31ba54fae707f0916fe172182
SoffiaSanchezz <ssofiiadayanna@gmail.com>
2026-03-07 01:37:28 -0500

    Casos de uso e ingreso y salida de la data de el login

--- src/app/feature/auth/core/usecases/login.usecase.ts
+++ src/app/feature/auth/core/usecases/login.usecase.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UseCase } from '@core/base/use-case';
-import { UserEntity } from '@auth-core/entities/user.entity';
+import { LoginResponseEntity } from '@auth-core/entities/login-response.entity';
+import { LoginRequestEntity } from '@auth-core/entities/login-information.entity';
 import { AuthRepository } from '@auth-core/repositories/auth.repository';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginUseCase implements UseCase<any, UserEntity> {
-  constructor(private repository: AuthRepository) {}
+export class LoginUseCase implements UseCase<LoginRequestEntity, LoginResponseEntity> {
+  constructor(private authRepository: AuthRepository) {}
 
-  execute(params: any): Observable<UserEntity> {
-    return this.repository.login(params);
+  execute(params: LoginRequestEntity): Observable<LoginResponseEntity> {
+    return this.authRepository.authenticateUser(params);
   }
 }
